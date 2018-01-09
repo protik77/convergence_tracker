531be2146e7b8bc74dfca1679b6ed99f1e520615
Protik Das <ptkapece@gmail.com>
2018-01-08 21:48:47 -0800

    finished up to convergence run

--- convergence_tracker.py
+++ convergence_tracker.py
@@ -1,4 +1,7 @@
 from ase.io import read
+from pymatgen.io.ase import AseAtomsAdaptor
+from math import ceil
+from ase.calculators.vasp import Vasp
 
 class ConvergenceTracker():
     ''' Class for tracking convergence.
@@ -18,6 +21,7 @@ class ConvergenceTracker():
         self.convergence_th = convergence_th
         self.structure_path = structure_path
         self.cutoff = cutoff  # in eV
+        self.max_iter = 500 # maximum number of iteration
 
     def read_structure(self):
         ''' Reads structure from the path given
@@ -28,34 +32,76 @@ class ConvergenceTracker():
 
         return read(self.structure_path)
 
+    def get_lattice_constants(self):
+        ''' Returns lattice constants as a list
+
+        '''
+
+        ase_struct = self.read_structure()
+
+        # convert ase object to pymatgen object
+        pmg_struct = AseAtomsAdaptor.get_structure(ase_struct)
+
+        lattice_constants = list(pmg_struct.lattice.abc)
+
+        # convert numpy data types to float to be consistent
+        lattice_constants = [float(a) for a in lattice_constants]
+
+        return lattice_constants
+
     def scale_kpoints(self):
         ''' Will return a list with scaling of correspondin k-point.
 
         If the lattice constant of the structure in any direction is twice or
         larger compared to any other direction, then k-points needed for that
         direction is half. This function will check for such properties and
-        sets a list for scaling.
+        returns a list for scaling the k-points.
         '''
 
-        pass
+        lat_const = self.get_lattice_constants()
+
+        return [ceil(z / min(lat_const)) for z in lat_const]
+
+    def get_new_kpoints(self, old_kpoints, scaling_factor, inc=2):
+        '''Gets new k-points from old k-points by increasing from the earlier
+        values and scaling the k-points using the scaling_factor.
+
+        '''
+
+        new_max = max(old_kpoints) + inc
+
+        new_kpoints = [ceil(new_max / a) for a in scaling_factor]
+
+        return new_kpoints
 
     def create_calculator(self):
-        '''Creates calculator object from calculator.
+        '''Creates calculator object using ASE.
 
         Reads mode from the calculator input and creates a calculator object
         with default parameters. In case changes needed in the default
         parameters, this class can be sub-classed.
+        If the calculator is not supported, NotImplementedError is raised.
         '''
 
-        pass
+        if self.calculator == 'vasp':
+            calc_obj = Vasp(pp='pbe',
+                    istart=0,
+                    ismear=1,
+                    sigma=0.05,
+                    lcharg=False,
+                    lwave=False,
+                    encut = self.cutoff,
+                    algo='Normal',
+                    nelm=200,
+                    ediff=1e-6,
+                    ibrion=-1,
+                    nsw=0,
+                    gamma=True)
 
-    def get_new_kpoints(self):
-        '''Gets new k-points from old k-points by increasing from the earlier
-        values and taking into account of the scaling.
+        else:
+            raise NotImplementedError('Calculator {} not implemented.'.format(self.calculator))
 
-        '''
-
-        pass
+        return calc_obj
 
     def run_convergence(self):
         '''Runs calculations and checks for convergence.
@@ -64,7 +110,37 @@ class ConvergenceTracker():
         Returns list of converged k-points.
         '''
 
-        pass
+        # dummy value for initial value of energy
+        initial_kpoints = [1, 1, 1]
+        old_kp = initial_kpoints
+        scaling_factor = self.scale_kpoints()
+
+        for iteration in range(self.max_iter):
+
+            structure = self.read_structure()
+
+            calc = self.create_calculator()
+
+            if iteration == 0:
+                this_kp = initial_kpoints
+            else:
+                this_kp = self.get_new_kpoints(old_kp, scaling_factor)
+
+            calc.set(kpts = this_kp)
+
+            structure.set_calculator(calc)
+
+            energy = structure.get_potential_energy()
+
+            if iteration > 0 and abs(energy - old_energy) < self.convergence_th :
+                return this_kp
+            else:
+                old_kp = this_kp
+                old_energy = energy
+
+            if iteration == self.max_iter-1:
+                raise RuntimeError('Reached maximum iteration without convergence.')
+
 
     def logger(self):
         '''Logs k-points and energy for each iteration.
